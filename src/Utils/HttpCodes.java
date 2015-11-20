cd3d4229b31e1381717f79c8c0c3e425effb35b1
Kevin Noonan <KJNoonan52@gmail.com>
2015-11-19 22:31:58 -0600

    Adds in a Helper Class to Store common codes, commands, and headers.

---
+++ src/Utils/HttpCodes.java
@@ -0,0 +1,16 @@
+package Utils;
+
+public class HttpCodes {
+	public static final String HEADER = "HTTP/1.1";
+	
+	public static class STATUSES {
+		public static final String OK = "200 OK";
+	}
+	
+	public static class COMMANDS {
+		public static final String SNYC = "SNYC";
+		public static final String UPLOAD = "UPLOAD";
+		public static final String CLOSE = "CLOSE";
+	}
+}
+
